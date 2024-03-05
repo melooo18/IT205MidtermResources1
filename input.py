89752c87564e3048fa9ea43d4c6d1dd31025c616
melooo18 <157451916+melooo18@users.noreply.github.com>
2024-03-05 15:35:17 +0800

    Add files via upload

---
+++ input.py
@@ -0,0 +1,9 @@
+#input() has string data type
+num1 = int(input("Enter 1st number: "))
+
+#int() for converting string to integers
+num2 = int(input("Enter 2nd number: "))
+
+sum = num1 + num2
+
+print(f"The sum of {num1} and {num2} is: {sum}")
---
+++ output.py
@@ -0,0 +1,14 @@
+#variable declaration
+x = 22
+message = "Hi po!"
+ratio = 3.14
+
+#checking the type of data with() method.
+print(type(message))
+print(type(ratio))
+print(type(x))
+
+#string formatting
+print(message + "My name is PJ. I am  " + str(x) + "years old.")
+print(f"{message} My name is PJ. I am {x} years old.")
+
